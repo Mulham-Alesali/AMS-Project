fd633c9dcb8771f4d841f3108fdf2644c6b96176
Mulham Alesali <MB3.EMAIL@GMAIL.COM>
2020-02-19 17:29:47 +0100

    Griffer öffnet und zu machen

--- Navigator.cpp
+++ Navigator.cpp
@@ -25,6 +25,7 @@ robots@mobilerobots.com or
 Adept MobileRobots, 10 Columbia Drive, Amherst, NH 03031; +1-603-881-7960
 */
 #include "Aria.h"
+#include <ArGripper.h>
 
 /** @example simpleMotionCommands.cpp example showing how to connect and send
  * basic motion commands to the robot
@@ -47,58 +48,147 @@ Adept MobileRobots, 10 Columbia Drive, Amherst, NH 03031; +1-603-881-7960
  * ethernet-serial bridge.)
  */
 
+
+
+// Adds key handler callbacks for controlling the gripper
+class GripperControlHandler
+{
+	ArGripper* myGripper;
+	ArFunctorC<GripperControlHandler> myUpCB;
+	ArFunctorC<GripperControlHandler> myDownCB;
+	ArFunctorC<GripperControlHandler> myOpenCB;
+	ArFunctorC<GripperControlHandler> myCloseCB;
+	ArFunctorC<GripperControlHandler> myStopCB;
+public:
+	GripperControlHandler(ArGripper* gripper) :
+		myGripper(gripper),
+		myUpCB(this, &GripperControlHandler::liftUp),
+		myDownCB(this, &GripperControlHandler::liftDown),
+		myOpenCB(this, &GripperControlHandler::open),
+		myCloseCB(this, &GripperControlHandler::close),
+		myStopCB(this, &GripperControlHandler::stop)
+	{
+	}
+	void addKeyHandlers(ArRobot *robot)
+	{
+		ArKeyHandler *keyHandler = Aria::getKeyHandler();
+		if (keyHandler == NULL)
+		{
+			keyHandler = new ArKeyHandler();
+			Aria::setKeyHandler(keyHandler);
+			robot->attachKeyHandler(keyHandler);
+		}
+		keyHandler->addKeyHandler(ArKeyHandler::PAGEUP, &myUpCB);
+		keyHandler->addKeyHandler('u', &myUpCB);
+		keyHandler->addKeyHandler(ArKeyHandler::PAGEDOWN, &myDownCB);
+		keyHandler->addKeyHandler('d', &myDownCB);
+		keyHandler->addKeyHandler('o', &myOpenCB);
+		keyHandler->addKeyHandler('c', &myCloseCB);
+		keyHandler->addKeyHandler('s', &myStopCB);
+	}
+	void liftUp()
+	{
+		ArLog::log(ArLog::Normal, "Moving gripper lift up...");
+		myGripper->liftUp();
+	}
+	void liftDown()
+	{
+		ArLog::log(ArLog::Normal, "Moving gripper lift down...");
+		myGripper->liftDown();
+	}
+	void stop()
+	{
+		ArLog::log(ArLog::Normal, "Stopping gripper...");
+		myGripper->gripperHalt(); // stops both lift an grip
+		//myGripper->liftStop(); // stops just the lift
+		//myGripper->gripStop(); // stops just the gripper
+	}
+	void close()
+	{
+		ArLog::log(ArLog::Normal, "Closing gripper...");
+		myGripper->gripClose();
+	}
+	void open()
+	{
+		ArLog::log(ArLog::Normal, "Opening gripper...");
+		myGripper->gripOpen();
+	}
+};
+
+ArRobot robot;
+
+void turnRight90(){
+	robot.lock();
+	robot.setDeltaHeading(90);
+	robot.unlock();
+	ArUtil::sleep(3000);
+}
+
+void turnLeft90(){
+	robot.lock();
+	robot.setDeltaHeading(-90);
+	robot.unlock();
+	ArUtil::sleep(3000);
+}
+
+
+
 int main(int argc, char **argv)
 {
 
-  Aria::init();
-  ArRobot robot;
-  ArArgumentParser parser(&argc, argv);
-  parser.loadDefaultArguments();
+	Aria::init();
 
-  ArLog::log(ArLog::Terse, "WARNING: this program does no sensing or avoiding of obstacles, the robot WILL collide with any objects in the way! Make sure the robot has approximately 3 meters of free space on all sides.");
 
-  // ArRobotConnector connects to the robot, get some initial data from it such as type and name,
-  // and then loads parameter files for this robot.
-  ArRobotConnector robotConnector(&parser, &robot);
-  if(!robotConnector.connectRobot())
-  {
-    ArLog::log(ArLog::Terse, "simpleMotionCommands: Could not connect to the robot.");
-    if(parser.checkHelpAndWarnUnparsed())
-    {
-        Aria::logOptions();
-        Aria::exit(1);
-        return 1;
-    }
-  }
-  if (!Aria::parseArgs())
-  {
-    Aria::logOptions();
-    Aria::exit(1);
-    return 1;
-  }
-  
-  ArLog::log(ArLog::Normal, "simpleMotionCommands: Connected.");
+	//gripper testing 
+	ArGripper gripper(&robot);
 
-  // Start the robot processing cycle running in the background.
-  // True parameter means that if the connection is lost, then the 
-  // run loop ends.
-  robot.runAsync(true);
 
-  /* Motore einschalten */
-  robot.lock();
-  robot.enableMotors();
-  robot.unlock();
 
-  for (int i = 0; i< 3; i++) {
+	ArArgumentParser parser(&argc, argv);
+	parser.loadDefaultArguments();
 
-	  /* Drehung 120 anweisen und 3 Sekunden warten */
-	  robot.lock();
-	  robot.setDeltaHeading(120);
-	  robot.unlock();
-	  ArUtil::sleep(3000);
-	  ArLog::log(ArLog::Normal, "simpleConnect: Turn finished ...");
-  }
+	ArLog::log(ArLog::Terse, "WARNING: this program does no sensing or avoiding of obstacles, the robot WILL collide with any objects in the way! Make sure the robot has approximately 3 meters of free space on all sides.");
 
+	// ArRobotConnector connects to the robot, get some initial data from it such as type and name,
+	// and then loads parameter files for this robot.
+	ArRobotConnector robotConnector(&parser, &robot);
+	if (!robotConnector.connectRobot())
+	{
+		ArLog::log(ArLog::Terse, "simpleMotionCommands: Could not connect to the robot.");
+		if (parser.checkHelpAndWarnUnparsed())
+		{
+			Aria::logOptions();
+			Aria::exit(1);
+			return 1;
+		}
+	}
+	if (!Aria::parseArgs())
+	{
+		Aria::logOptions();
+		Aria::exit(1);
+		return 1;
+	}
+
+	ArLog::log(ArLog::Normal, "simpleMotionCommands: Connected.");
+
+	// Start the robot processing cycle running in the background.
+	// True parameter means that if the connection is lost, then the 
+	// run loop ends.
+	robot.runAsync(true);
+
+	/* Motore einschalten */
+	robot.lock();
+	robot.enableMotors();
+	robot.unlock();
+
+	gripper.gripOpen();
+	ArUtil::sleep(10000);
+	gripper.gripClose();
+
+	turnRight90();
+	turnRight90();
+	turnLeft90();
+	turnLeft90();
 
   ArLog::log(ArLog::Normal, "simpleMotionCommands: Exiting.");
   Aria::exit(0);
