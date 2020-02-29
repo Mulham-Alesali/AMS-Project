46c2333f4cbcd3dda4136cce50271b2f732b294c
Mulham Alesali <MB3.EMAIL@GMAIL.COM>
2020-02-29 19:02:44 +0100

    Update Navigator

--- Navigator.cpp
+++ Navigator.cpp
@@ -19,9 +19,9 @@ Copyright (C) 2016 Omron Adept Technologies, Inc.
      along with this program; if not, write to the Free Software
      Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
-If you wish to redistribute ARIA under different terms, contact 
-Adept MobileRobots for information about a commercial version of ARIA at 
-robots@mobilerobots.com or 
+If you wish to redistribute ARIA under different terms, contact
+Adept MobileRobots for information about a commercial version of ARIA at
+robots@mobilerobots.com or
 Adept MobileRobots, 10 Columbia Drive, Amherst, NH 03031; +1-603-881-7960
 */
 #include "Aria.h"
@@ -34,7 +34,7 @@ Adept MobileRobots, 10 Columbia Drive, Amherst, NH 03031; +1-603-881-7960
  * actions. This example shows direct motion commands. See actionExample.cpp,
  * actionGroupExample.cpp, and others for examples on how to use actions.
  * Actions provide a more modular way of performing more complex motion
- * behaviors than the simple imperitive style used here.  
+ * behaviors than the simple imperitive style used here. 
  *
  * See the ArRobot class documentation, as well as the overview of robot motion,
  * for more information.
@@ -53,142 +53,261 @@ Adept MobileRobots, 10 Columbia Drive, Amherst, NH 03031; +1-603-881-7960
 // Adds key handler callbacks for controlling the gripper
 class GripperControlHandler
 {
-	ArGripper* myGripper;
-	ArFunctorC<GripperControlHandler> myUpCB;
-	ArFunctorC<GripperControlHandler> myDownCB;
-	ArFunctorC<GripperControlHandler> myOpenCB;
-	ArFunctorC<GripperControlHandler> myCloseCB;
-	ArFunctorC<GripperControlHandler> myStopCB;
+    ArGripper* myGripper;
+    ArFunctorC<GripperControlHandler> myUpCB;
+    ArFunctorC<GripperControlHandler> myDownCB;
+    ArFunctorC<GripperControlHandler> myOpenCB;
+    ArFunctorC<GripperControlHandler> myCloseCB;
+    ArFunctorC<GripperControlHandler> myStopCB;
 public:
-	GripperControlHandler(ArGripper* gripper) :
-		myGripper(gripper),
-		myUpCB(this, &GripperControlHandler::liftUp),
-		myDownCB(this, &GripperControlHandler::liftDown),
-		myOpenCB(this, &GripperControlHandler::open),
-		myCloseCB(this, &GripperControlHandler::close),
-		myStopCB(this, &GripperControlHandler::stop)
-	{
-	}
-	void addKeyHandlers(ArRobot *robot)
-	{
-		ArKeyHandler *keyHandler = Aria::getKeyHandler();
-		if (keyHandler == NULL)
-		{
-			keyHandler = new ArKeyHandler();
-			Aria::setKeyHandler(keyHandler);
-			robot->attachKeyHandler(keyHandler);
-		}
-		keyHandler->addKeyHandler(ArKeyHandler::PAGEUP, &myUpCB);
-		keyHandler->addKeyHandler('u', &myUpCB);
-		keyHandler->addKeyHandler(ArKeyHandler::PAGEDOWN, &myDownCB);
-		keyHandler->addKeyHandler('d', &myDownCB);
-		keyHandler->addKeyHandler('o', &myOpenCB);
-		keyHandler->addKeyHandler('c', &myCloseCB);
-		keyHandler->addKeyHandler('s', &myStopCB);
-	}
-	void liftUp()
-	{
-		ArLog::log(ArLog::Normal, "Moving gripper lift up...");
-		myGripper->liftUp();
-	}
-	void liftDown()
-	{
-		ArLog::log(ArLog::Normal, "Moving gripper lift down...");
-		myGripper->liftDown();
-	}
-	void stop()
-	{
-		ArLog::log(ArLog::Normal, "Stopping gripper...");
-		myGripper->gripperHalt(); // stops both lift an grip
-		//myGripper->liftStop(); // stops just the lift
-		//myGripper->gripStop(); // stops just the gripper
-	}
-	void close()
-	{
-		ArLog::log(ArLog::Normal, "Closing gripper...");
-		myGripper->gripClose();
-	}
-	void open()
-	{
-		ArLog::log(ArLog::Normal, "Opening gripper...");
-		myGripper->gripOpen();
-	}
+    GripperControlHandler(ArGripper* gripper) :
+        myGripper(gripper),
+        myUpCB(this, &GripperControlHandler::liftUp),
+        myDownCB(this, &GripperControlHandler::liftDown),
+        myOpenCB(this, &GripperControlHandler::open),
+        myCloseCB(this, &GripperControlHandler::close),
+        myStopCB(this, &GripperControlHandler::stop)
+    {
+    }
+    void addKeyHandlers(ArRobot *robot)
+    {
+        ArKeyHandler *keyHandler = Aria::getKeyHandler();
+        if (keyHandler == NULL)
+        {
+            keyHandler = new ArKeyHandler();
+            Aria::setKeyHandler(keyHandler);
+            robot->attachKeyHandler(keyHandler);
+        }
+        keyHandler->addKeyHandler(ArKeyHandler:AGEUP, &myUpCB);
+        keyHandler->addKeyHandler('u', &myUpCB);
+        keyHandler->addKeyHandler(ArKeyHandler:AGEDOWN, &myDownCB);
+        keyHandler->addKeyHandler('d', &myDownCB);
+        keyHandler->addKeyHandler('o', &myOpenCB);
+        keyHandler->addKeyHandler('c', &myCloseCB);
+        keyHandler->addKeyHandler('s', &myStopCB);
+    }
+    void liftUp()
+    {
+        ArLog::log(ArLog::Normal, "Moving gripper lift up...");
+        myGripper->liftUp();
+    }
+    void liftDown()
+    {
+        ArLog::log(ArLog::Normal, "Moving gripper lift down...");
+        myGripper->liftDown();
+    }
+    void stop()
+    {
+        ArLog::log(ArLog::Normal, "Stopping gripper...");
+        myGripper->gripperHalt(); // stops both lift an grip
+        //myGripper->liftStop(); // stops just the lift
+        //myGripper->gripStop(); // stops just the gripper
+    }
+    void close()
+    {
+        ArLog::log(ArLog::Normal, "Closing gripper...");
+        myGripper->gripClose();
+    }
+    void open()
+    {
+        ArLog::log(ArLog::Normal, "Opening gripper...");
+        myGripper->gripOpen();
+    }
 };
 
 ArRobot robot;
+ArGripper gripper(&robot);
 
 void turnRight90(){
-	robot.lock();
-	robot.setDeltaHeading(90);
-	robot.unlock();
-	ArUtil::sleep(3000);
+    robot.lock();
+    robot.setDeltaHeading(90);
+    robot.unlock();
+    ArUtil::sleep(3000);
 }
 
 void turnLeft90(){
-	robot.lock();
-	robot.setDeltaHeading(-90);
-	robot.unlock();
-	ArUtil::sleep(3000);
+    robot.lock();
+    robot.setDeltaHeading(-90);
+    robot.unlock();
+    ArUtil::sleep(3000);
 }
 
+void greifOben(){
+    gripper.liftUp();
+    gripper.gripOpen();
+    /*while (!(gripper.isLiftMaxed() && gripper.getGripState() == 1)){
+    ArUtil::sleep(100);
+    }*/
+    ArUtil::sleep(5000);
+
+    gripper.liftDown();
+    ArUtil::sleep(2000);
+    gripper.liftStop();
+    gripper.gripClose();
+
+    gripper.liftUp();
+    while (!gripper.isLiftMaxed()){
+        ArUtil::sleep(100);
+    }
+}
+
+void greifUnten(){
+    gripper.liftUp();
+    gripper.gripOpen();
+    while (!(gripper.isLiftMaxed() && gripper.getGripState() == 1)){
+        ArUtil::sleep(100);
+    }
+
+    gripper.liftDown();
+   
+    while (gripper.isLiftMaxed()){
+        ArUtil::sleep(100);
+    }
+
+    gripper.liftStop();
+    gripper.gripClose();
+
+    gripper.liftUp();
+    while (!gripper.isLiftMaxed()){
+        ArUtil::sleep(100);
+    }
+
+}
+
+void lassenOben(){
+    gripper.gripOpen();
+    ArUtil::sleep(3000);
+}
+
+void lassenUnten(){
+    gripper.gripOpen();
+    ArUtil::sleep(3000);
+}
 
 
 int main(int argc, char **argv)
 {
 
-	Aria::init();
+    Aria::init();
 
 
-	//gripper testing 
-	ArGripper gripper(&robot);
+    //gripper testing
 
 
 
-	ArArgumentParser parser(&argc, argv);
-	parser.loadDefaultArguments();
+    ArArgumentParser parser(&argc, argv);
+    parser.loadDefaultArguments();
 
-	ArLog::log(ArLog::Terse, "WARNING: this program does no sensing or avoiding of obstacles, the robot WILL collide with any objects in the way! Make sure the robot has approximately 3 meters of free space on all sides.");
+    ArLog::log(ArLog::Terse, "WARNING: this program does no sensing or avoiding of obstacles, the robot WILL collide with any objects in the way! Make sure the robot has approximately 3 meters of free space on all sides.");
 
-	// ArRobotConnector connects to the robot, get some initial data from it such as type and name,
-	// and then loads parameter files for this robot.
-	ArRobotConnector robotConnector(&parser, &robot);
-	if (!robotConnector.connectRobot())
-	{
-		ArLog::log(ArLog::Terse, "simpleMotionCommands: Could not connect to the robot.");
-		if (parser.checkHelpAndWarnUnparsed())
-		{
-			Aria::logOptions();
-			Aria::exit(1);
-			return 1;
-		}
-	}
-	if (!Aria::parseArgs())
-	{
-		Aria::logOptions();
-		Aria::exit(1);
-		return 1;
-	}
+    // ArRobotConnector connects to the robot, get some initial data from it such as type and name,
+    // and then loads parameter files for this robot.
+    ArRobotConnector robotConnector(&parser, &robot);
+    if (!robotConnector.connectRobot())
+    {
+        ArLog::log(ArLog::Terse, "simpleMotionCommands: Could not connect to the robot.");
+        if (parser.checkHelpAndWarnUnparsed())
+        {
+            Aria::logOptions();
+            Aria::exit(1);
+            //return 1;
+        }
+    }
+    if (!Aria::parseArgs() || !parser.checkHelpAndWarnUnparsed())
+    {
+        Aria::logOptions();
+        Aria::exit(1);
+        //return 1;
+    }
 
-	ArLog::log(ArLog::Normal, "simpleMotionCommands: Connected.");
+    ArLog::log(ArLog::Normal, "simpleMotionCommands: Connected.");
 
-	// Start the robot processing cycle running in the background.
-	// True parameter means that if the connection is lost, then the 
-	// run loop ends.
-	robot.runAsync(true);
+    // Start the robot processing cycle running in the background.
+    // True parameter means that if the connection is lost, then the
+    // run loop ends.
+   
+    robot.runAsync(true);
+   
+    robot.lock();
+    robot.enableMotors();
+    robot.unlock();
 
-	/* Motore einschalten */
-	robot.lock();
-	robot.enableMotors();
-	robot.unlock();
+    greifOben();
+    turnLeft90();
+    lassenOben();
+    turnRight90();
+    greifUnten();
+    turnLeft90();
+    turnRight90();
 
-	gripper.gripOpen();
-	ArUtil::sleep(10000);
-	gripper.gripClose();
 
-	turnRight90();
-	turnRight90();
-	turnLeft90();
-	turnLeft90();
+    /* Motore einschalten
+    gripper.liftUp();
+    gripper.gripOpen();
+    //ArUtil::sleep(10000);
+    ArUtil::sleep(7000);
+    gripper.liftDown();
+    ArUtil::sleep(2000);
+    gripper.liftStop();
+    gripper.gripClose();
+
+    ArUtil::sleep(5000);
+    gripper.liftUp();
+    turnRight90();
+    gripper.gripOpen();
+    ArUtil::sleep(5000);
+
+    turnLeft90();
+    gripper.liftDown();
+    ArUtil::sleep(5000);
+    gripper.gripClose();
+    ArUtil::sleep(2000);
+
+    gripper.liftUp();
+    ArUtil::sleep(5000);
+    turnRight90();
+    gripper.gripOpen();
+    ArUtil::sleep(3000);
+    turnLeft90();
+
+   
+    */
+
+
+   
+
+    gripper.liftStop();
+
+    //gripper.liftDown();
+
+
+    //turnRight90();
+    //nawid bitte hier arbeiten
+    //robot.lock();
+    //gripper.gripOpen();
+    //robot.unlock();
+    /*ArUtil::sleep(2000);
+    gripper.gripStop();
+   
+    gripper.gripClose();
+    ArUtil::sleep(1000);
+    gripper.gripStop();*/
+
+
+
+
+    //gripper.liftUp();
+    //ArUtil::sleep(10000);
+    //gripper.liftDown();
+    //ArUtil::sleep(10000);
+    //gripper.gripOpen();
+    //ArUtil::sleep(10000);
+    //gripper.gripClose();
+
+    //turnRight90();
+    //turnRight90();
+    //turnLeft90();
+    //turnLeft90();
 
   ArLog::log(ArLog::Normal, "simpleMotionCommands: Exiting.");
   Aria::exit(0);
